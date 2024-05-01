1a761cbc084e9ff8d7b41656e90ddc0a5972f8ed
SKele10 <shoaib.sk.10.sk@gmail.com>
2024-04-30 21:37:05 -0400

    Title and favicon changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { Oswald } from "next/font/google";
@@ -7,6 +8,10 @@ const oswald = Oswald({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
     <div className={oswald.className}>
+      <Head>
+        <title>SpaceXplorer</title>
+        <link rel="icon" href="/spacex.png" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
